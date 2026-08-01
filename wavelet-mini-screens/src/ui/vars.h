71199559fc6ce3d066af8b3db4fbdfec59f45dd0
Semire Ajayi <semajayi1234@gmail.com>
2026-08-01 17:38:30 +0100

    Started coding UI for Wavelet Mini

---
+++ wavelet-mini-screens/src/ui/vars.h
@@ -0,0 +1,29 @@
+#ifndef EEZ_LVGL_UI_VARS_H
+#define EEZ_LVGL_UI_VARS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// enum declarations
+
+// Flow global variables
+
+enum FlowGlobalVariables {
+    FLOW_GLOBAL_VARIABLE_SETUP_DONE = 0,
+    FLOW_GLOBAL_VARIABLE_COLOR_THEME = 1,
+    FLOW_GLOBAL_VARIABLE_GENERATED_PAYLOAD_TOKEN = 2,
+    FLOW_GLOBAL_VARIABLE_SUCCESSFUL_PAIR = 3,
+    FLOW_GLOBAL_VARIABLE_IS_SPOTIFY_PLAYING = 4
+};
+
+// Native global variables
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_VARS_H*/
